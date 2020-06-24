f5ac36690f733168bc050d4f06725efa8a1d4d06
Rawand Saeed <rawand.saeed@gmail.com>
2020-06-24 15:06:12 +0300

    Added Login Activity

--- app/src/main/java/com/example/slackclone/MainActivity.kt
+++ app/src/main/java/com/example/slackclone/MainActivity.kt
@@ -1,45 +1,49 @@
 package com.example.slackclone
 
+import android.content.Intent
 import android.os.Bundle
-import android.view.Gravity
-import android.view.Menu
+
 import android.view.View
 import androidx.appcompat.app.ActionBarDrawerToggle
-import com.google.android.material.floatingactionbutton.FloatingActionButton
-import com.google.android.material.snackbar.Snackbar
 import com.google.android.material.navigation.NavigationView
-import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.navigateUp
-import androidx.navigation.ui.setupActionBarWithNavController
-import androidx.navigation.ui.setupWithNavController
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
 import androidx.core.view.GravityCompat
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.app_bar_main.*
 
 class MainActivity : AppCompatActivity() {
-
-
-    private lateinit var drawerLayout: DrawerLayout
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val toolbar: Toolbar = findViewById(R.id.toolbar)
         setSupportActionBar(toolbar)
 
-        drawerLayout = findViewById(R.id.drawer_layout)
-        val navView: NavigationView = findViewById(R.id.nav_view)
-
         val toggle = ActionBarDrawerToggle(
-            this, drawerLayout, toolbar, R.string.openNavDrawer, R.string.closeNavDrawer )
-        drawerLayout.addDrawerListener(toggle)
+            this, drawer_layout, toolbar, R.string.openNavDrawer, R.string.closeNavDrawer )
+        drawer_layout.addDrawerListener(toggle)
         toggle.syncState()
+
     }
 
+    override fun onBackPressed() {
+        if (drawer_layout.isDrawerOpen(GravityCompat.START)) {
+            drawer_layout.closeDrawer(GravityCompat.START)
+        } else {
+            super.onBackPressed()
+        }
+    }
 
-    fun loginBtnNavClicked(view: View) {}
-    fun addChannelClicked(view: View) {}
-    fun sendMsgBtnClicked(view: View) {}
+    fun loginBtnNavClicked(view: View) {
+        val loginIntent = Intent(this, LoginActivity::class.java)
+        startActivity(loginIntent)
+
+    }
+    fun addChannelClicked(view: View) {
+    }
+    fun sendMsgBtnClicked(view: View) {
+        val loginIntent = Intent(this, LoginActivity::class.java)
+        startActivity(loginIntent)
+    }
 }
